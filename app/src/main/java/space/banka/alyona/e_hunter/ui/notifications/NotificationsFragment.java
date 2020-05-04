df071402a8e24dc139a399b5d1b3568462a4f320
Alyona Kovalyova <alyona+github@banka.space>
2020-05-04 13:45:09 +0300

    Add first version of application

---
+++ app/src/main/java/space/banka/alyona/e_hunter/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,35 @@
+package space.banka.alyona.e_hunter.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import space.banka.alyona.e_hunter.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
